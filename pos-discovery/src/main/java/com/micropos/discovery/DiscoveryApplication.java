26b608d2c2d78b9d6a35ffd5b0ae32ae640746c4
ZFirozen <zfirozen@163.com>
2022-07-07 03:17:17 +0800

    Do some changes to Discovery
    to fit previous changes.

--- pos-discovery/src/main/java/com/micropos/discovery/DiscoveryApplication.java
+++ pos-discovery/src/main/java/com/micropos/discovery/DiscoveryApplication.java
@@ -1,15 +1,14 @@
 package com.micropos.discovery;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 }
